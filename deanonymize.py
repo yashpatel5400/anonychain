99f9302c6f266a6521baf47a4783cf471e2106d5
Yash Patel <ypatel5400@gmail.com>
2017-11-22 16:45:38 -0500

    Added cluster support and eigenvalue graphs

--- deanonymize.py
+++ deanonymize.py
@@ -16,18 +16,20 @@ def main(argv):
     q            = 0.25
     cluster_size = 10
     num_clusters = 2
-    lib          = "plotly"
+    cs           = None
+    lib          = "matplotlib"
 
     USAGE_STRING = """eigenvalues.py 
-            -d <display_bool> [(y/n) for whether to show PCA projections]
-            -c <cluster_size> [(int) size of each cluster (assumed to be same for all)]
-            -n <num_cluster>  [(int) number of clusters (distinct people)]
-            -p <p_value>      [(0,1) for in-cluster probability]
-            -q <q_value>      [(0,1) for non-cluster probability] 
-            --lib             [('matplotlib','plotly') for plotting library]"""
+            -d <display_bool>    [(y/n) for whether to show PCA projections]
+            -c <cluster_size>    [(int) size of each cluster (assumed to be same for all)]
+            -n <num_cluster>     [(int) number of clusters (distinct people)]
+            -p <p_value>         [(0,1) for in-cluster probability]
+            -q <q_value>         [(0,1) for non-cluster probability] 
+            --cs <cluster_sizes> [(int lisst) size of each cluster (comma delimited)]
+            --lib                [('matplotlib','plotly') for plotting library]"""
 
     try:
-        opts, args = getopt.getopt(argv,"d:c:n:p:q:",['lib='])
+        opts, args = getopt.getopt(argv,"d:c:n:p:q:",['lib=','cs='])
     except getopt.GetoptError:
         print("Using default values. To change use: \n{}".format(USAGE_STRING))
 
@@ -40,9 +42,14 @@ def main(argv):
         elif opt in ("-q"): q = float(arg)
         elif opt in ("-c"): cluster_size = int(arg)
         elif opt in ("-n"): num_clusters = int(arg)
+        elif opt in ("--cs"):  cs = arg
         elif opt in ("--lib"): lib = arg
 
-    cluster_sizes = [cluster_size] * num_clusters
+    if cs is not None:
+        cluster_sizes = [int(cluster) for cluster in cs.split(",")]
+    else:
+        cluster_sizes = [cluster_size] * num_clusters
+    
     sbm = create_sbm(cluster_sizes, p, q)
     if pca == "y":
         plot_pca(sbm, cluster_sizes, plot_2d=True, plot_3d=True, plot_lib=lib)
