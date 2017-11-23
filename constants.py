96e7873d824b16df1e9d13ba5add908d91640e20
Yash Patel <ypatel@princeton.edu>
2017-11-23 13:15:05 -0500

    Standardized colors

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+__author__ = Yash Patel
+__name__   = constants.py
+__description__ = List of constants
+"""
+
+from matplotlib import colors as mcolors
+
+colors = list(mcolors.CSS4_COLORS.values())
