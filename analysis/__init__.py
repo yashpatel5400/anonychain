f33f59b9b94402c68bc6bd30308f5523f5d39fe4
Yash Patel <ypatel@princeton.edu>
2017-11-28 20:10:19 -0500

    Refactored codebase

---
+++ analysis/__init__.py
@@ -0,0 +1,5 @@
+"""
+__author__ = Yash Patel
+__name__   = __init__.py
+__description__ = Init for analysis part of package
+"""
---
+++ setup/__init__.py
@@ -0,0 +1,5 @@
+"""
+__author__ = Yash Patel
+__name__   = __init__.py
+__description__ = Init for setup part of package
+"""
