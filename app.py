bbffadffbb59b8350402476f589d6c7d03324034
Yash Patel <ypatel@princeton.edu>
2017-12-11 16:54:26 -0500

    Refactored code to make params dictionary

--- app.py
+++ app.py
@@ -16,19 +16,21 @@ from analysis.pca import plot_pca
 from analysis.deanonymize import draw_partitions, calc_accuracy, deanonymize
 
 def _cmd_graph(argv):
-    pca             = "y"
-    p               = 0.75
-    q               = 0.25
-
-    guess_clusters  = True 
-    cluster_size    = 10
-    num_clusters    = 2
-
-    cs              = None
-    lib             = "matplotlib"
+    params = {
+        "pca"             : "y",
+        "p"               : 0.75,
+        "q"               : 0.25,
+        "guess_clusters"  : True ,
+        "weighted"        : True,
+        "cluster_size"    : 10,
+        "num_clusters"    : 2,
+        "cs"              : None,
+        "lib"             : "matplotlib"
+    }
 
     USAGE_STRING = """eigenvalues.py 
             -d <display_bool>    [(y/n) for whether to show PCA projections]
+            -w <weighted_graph>  [(y/n) for whether to have weights on edges (randomized)]
             -c <cluster_size>    [(int) size of each cluster (assumed to be same for all)]
             -n <num_cluster>     [(int) number of clusters (distinct people)]
             -g <guess_bool>      [(y/n) to guess the number of clusters vs. take it as known] 
@@ -38,7 +40,7 @@ def _cmd_graph(argv):
             --lib                [('matplotlib','plotly') for plotting library]"""
 
     try:
-        opts, args = getopt.getopt(argv,"d:c:n:g:p:q:",['lib=','cs='])
+        opts, args = getopt.getopt(argv,"hd:c:n:g:p:q:",['lib=','cs='])
     except getopt.GetoptError:
         print("Using default values. To change use: \n{}".format(USAGE_STRING))
 
@@ -46,31 +48,33 @@ def _cmd_graph(argv):
         if opt in ('-h'):
             print(USAGE_STRING)
             sys.exit()
-        elif opt in ("-d"): pca = arg
-        elif opt in ("-c"): cluster_size = int(arg)
-        elif opt in ("-n"): num_clusters = int(arg)
-        elif opt in ("-g"): guess_clusters = (arg == "y")
+        elif opt in ("-d"): params["pca"] = arg
+        elif opt in ("-w"): params["weighted"] = arg
+        elif opt in ("-c"): params["cluster_size"] = int(arg)
+        elif opt in ("-n"): params["num_clusters"] = int(arg)
+        elif opt in ("-g"): params["guess_clusters"] = (arg == "y")
         
-        elif opt in ("-p"): p = float(arg)
-        elif opt in ("-q"): q = float(arg)
+        elif opt in ("-p"): params["p"] = float(arg)
+        elif opt in ("-q"): params["q"] = float(arg)
         
-        elif opt in ("--cs"):  cs = arg
-        elif opt in ("--lib"): lib = arg
+        elif opt in ("--cs"):  params["cs"] = arg
+        elif opt in ("--lib"): params["lib"] = arg
 
-    if cs is not None:
-        cluster_sizes = [int(cluster) for cluster in cs.split(",")]
+    if params["cs"] is not None:
+        params["cluster_sizes"] = [int(cluster) for cluster in params["cs"].split(",")]
     else:
-        cluster_sizes = [cluster_size] * num_clusters
+        params["cluster_sizes"] = [cluster_size] * num_clusters
 
-    clusters = create_clusters(cluster_sizes)
-    return clusters, guess_clusters, pca, p, q, lib
+    params["clusters"] = create_clusters(params["cluster_sizes"])
+    return params
 
 def main(argv):
-    clusters, guess_clusters, pca, p, q, lib = _cmd_graph(argv)
-    sbm = create_sbm(clusters, p, q)
-    plot_pca(sbm, clusters, plot_2d=True, plot_3d=True, plot_lib=lib)
+    params = _cmd_graph(argv)
+    clusters = params["clusters"]
+    sbm = create_sbm(clusters, params["p"], params["q"], params["weighted"])
+    plot_pca(sbm, clusters, plot_2d=True, plot_3d=True, plot_lib=params["lib"])
     
-    if guess_clusters:
+    if params["guess_clusters"]:
         hier_partitions, kmeans_partitions = deanonymize(sbm, k=None)
     else: 
         hier_partitions, kmeans_partitions = deanonymize(sbm, k=len(clusters))
