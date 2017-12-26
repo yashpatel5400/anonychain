2e14b16b06cdfe4031c7b9a7cdf1dd063e4f7b56
Yash Patel <ypatel5400@gmail.com>
2017-12-26 13:07:55 -0500

    Tested contraction; added folder structure

---
+++ sparsify/spectral.py
@@ -0,0 +1,14 @@
+"""
+__author__ = Yash Patel
+__name__   = spectral.py
+__description__ = Spectral sparsification of graphs
+"""
+
+def sparsify(G):
+    pass
+
+def main():
+    sparsify(G)
+
+if __name__ == "__main__":
+    main()
