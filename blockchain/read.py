e1a9d91a2e966ad9ebd38e1c411391322f6db426
Yash Patel <ypatel5400@gmail.com>
2017-12-26 16:23:50 -0500

    Added visualization script

--- blockchain/read.py
+++ blockchain/read.py
@@ -5,6 +5,7 @@ __description__ = Part that constructs the graph given the input data dump
 """
 
 import subprocess
+import struct
 
 import networkx as nx
 from networkx.drawing.nx_agraph import write_dot
@@ -12,14 +13,16 @@ from networkx.drawing.nx_agraph import write_dot
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import json
 
 def _read_in_chunks(file_object, chunk_size=9000):
     i=0
     while True:
-        if i % 1000 == 0:
-            print(i)
         data = file_object.read(chunk_size)
         i += 1
+        if i % 1000 == 0:
+            print(i)
+        
         if not data:
             break
         yield data
@@ -67,5 +70,34 @@ def create_simple_graph(fn):
                 G.add_edge(address1ID, address2ID, weight=1)
     return G
 
+def create_visual_json(fn):
+    f = open(fn, "rb")
+    data  = {}
+    data["nodes"] = []
+    data["links"] = []
+    nodes_to_ind = {}
+
+    with open("visualize/nodes.txt", 'w') as nodes_f:
+        with open("visualize/edges.txt", 'w') as edges_f:
+            for chunk in _read_in_chunks(f):
+                # raw format: address1ID (4 bytes) address2ID (4 bytes) Heuristics(1 byte)
+                for sequence_start in range(0, len(chunk), 9):
+                    sequence = chunk[sequence_start:sequence_start+9]
+                    address1ID, address2ID, heuristic = struct.unpack('iib', sequence)
+                    
+                    for addressID in [address1ID, address2ID]:
+                        if addressID not in nodes_to_ind:
+                            nodes_to_ind[addressID] = len(data["nodes"])
+                            data["nodes"].append({"name" : addressID})
+                            
+                    data["links"].append({
+                        "source": nodes_to_ind[address1ID],
+                        "target": nodes_to_ind[address2ID],
+                        "weight": heuristic
+                    })
+
+    with open("visualize/graphFile.json", "w") as dest:
+        json.dump(data, dest)
+                    
 if __name__ == "__main__":
-    G = create_simple_graph("graph.dat")
+    G = create_visual_json("blockchain/mini.dat")
