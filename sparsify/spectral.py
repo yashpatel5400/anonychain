4fed203efa492cef7ce8469549358edca3df030b
Yash Patel <ypatel5400@gmail.com>
2017-12-28 12:59:20 -0500

    Refactored and tested sparsification code

--- sparsify/spectral.py
+++ sparsify/spectral.py
@@ -9,16 +9,27 @@ import networkx as nx
 import random
 
 class SpectralSparsifier:
+    """Spectral sparsifer class (https://www.cs.ubc.ca/~nickhar/Cargese3.pdf), that
+    is intended to selectively remove edges from the graph without significantly
+    perturbing the Laplacian of the input graph. Initialized with an epsilon parameter
+    that indicates roughly how much tolerance is accetable on the graph Laplacian
+    """
     def __init__(self, epsilon=.5):
         self.epsilon = epsilon
 
     def sparsify(self, G):
+        """Given an input graph, deletes edges from the graph (NOT the same
+        as contracting edges). Action mutates original graph that is passed in
+
+        Returns void
+        """
         n = nx.number_of_nodes(G)
         L = nx.laplacian_matrix(G).todense()
         L_pinv = np.linalg.pinv(L)
 
         expected_nonzero = int(6 * n * np.log(n) / (self.epsilon ** 2))
-        print("Expected removals: {}".format(nx.number_of_edges(G) - expected_nonzero))
+        print("Expected removals: {} (Originally {})".format(
+            nx.number_of_edges(G) - expected_nonzero, nx.number_of_edges(G)))
 
         rho = int(6 * np.log(n) / (self.epsilon ** 2))
         to_remove = []
