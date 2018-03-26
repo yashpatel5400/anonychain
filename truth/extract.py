e60ed3496190e2034facc236629ae0f5fd9062df
Yash Patel <ypatel@princeton.edu>
2018-03-26 19:45:37 -0400

    Completed extraction code

--- truth/extract.py
+++ truth/extract.py
@@ -5,7 +5,8 @@ __description__ = Extracts the information of the ownership of wallets from the 
 provided in the Fistful of Bitcoin paper to that employed here. 
 """
 
-import csv
+import numpy as np
+import pandas as pd
 
 def extract_from_raw(fn):
     """
@@ -18,4 +19,46 @@ def extract_from_raw(fn):
     6th column : receiver public key
     7th column : label for that key.  
     """
-    
+    df = pd.read_csv(fn)
+    columns = ["tx_hash","sender_id","sender_key","sender_label",
+        "receiver_id","receiver_key","receiver_label"]
+    label_to_wallets = {}
+
+    senders   = np.unique(df["sender_label"])
+    receivers = np.unique(df["receiver_label"])
+
+    for sender in senders:
+        sender_txs = df[df["sender_label"] == sender]
+        label_to_wallets[sender] = set(sender_txs["sender_key"])
+
+    for receiver in receivers:
+        receiver_txs = df[df["receiver_label"] == receiver]
+
+        # special case when dealing with change addresses to associate them back to 
+        # the original person who made the transaction
+        if "change" in receiver:
+            np_receiver_txs = np.array(receiver_txs)
+            for receiver_tx in np_receiver_txs:
+                sender = receiver_tx[3]
+                receiver_key = receiver_tx[5]
+                if sender not in label_to_wallets:
+                    label_to_wallets[sender] = { receiver_key }
+                else:
+                    label_to_wallets[sender].add(receiver_key)
+
+        else:
+            receiver_keys = set(receiver_txs["receiver_key"])
+            if receiver not in label_to_wallets:
+                label_to_wallets[receiver] = receiver_keys
+            else:
+                label_to_wallets[receiver] = label_to_wallets[receiver].union(receiver_keys)
+    return label_to_wallets
+
+def write_extracted(fn):
+    label_to_wallets = extract_from_raw(fn)
+    with open("truth/extracted.txt", "w") as f:
+        for person in label_to_wallets:
+            f.write("{} : {}\n".format(person, label_to_wallets[person]))
+
+if __name__ == "__main__":
+    write_extracted("truth/our_txs.csv")
