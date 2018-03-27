e5ebe9d7bfe14b41731f373072fae9719b73c0d5
Yash Patel <ypatel@princeton.edu>
2018-03-27 16:37:46 -0400

    Added METIS to main pipeline

--- blockchain/metis.py
+++ blockchain/metis.py
@@ -8,7 +8,7 @@ analysis by the METIS package
 import subprocess
 import pickle
 import networkx as nx
-from scipy.sparse import dok_matrix
+from scipy.sparse import dok_matrix, csr_matrix
 import matplotlib
 matplotlib.use('Agg')
 
@@ -16,7 +16,11 @@ from analysis.deanonymize import draw_results
 
 def format_metis(S, metis_fn):
     rows, cols = S.shape
-    edges = dok_matrix.count_nonzero(S) // 2
+    if   isinstance(S, dok_matrix):
+        edges = dok_matrix.count_nonzero(S) // 2
+    elif isinstance(S, csr_matrix):
+        edges = csr_matrix.count_nonzero(S) // 2
+
     with open(metis_fn, "w") as f:
         # header format: #nodes, #edges, 001 (indicates weighted edges)
         f.write("{} {} 001\n".format(rows, edges))
@@ -27,14 +31,7 @@ def format_metis(S, metis_fn):
                     cur_node.append("{} {}".format(j+1, int(S[i,j])))
             f.write("{}\n".format(" ".join(cur_node)))
 
-def metis_partition(fn, num_partitions):
-    print("Loading pickled data...")
-    S = pickle.load(open("blockchain/{}.pickle".format(fn), "rb"))
-    metis_fn = "blockchain/{}.graph".format(fn)
-    
-    print("Reformatting pickled data...")
-    format_metis(S, metis_fn)
-
+def run_metis(metis_fn, num_partitions):
     pmetis_cmd = ["pmetis", metis_fn, str(num_partitions)]
     result = subprocess.run(pmetis_cmd)
 
@@ -45,10 +42,16 @@ def metis_partition(fn, num_partitions):
         for line in f:
             partitions[int(line.strip())].add(line_no)
             line_no += 1
+    return partitions
 
-    G = nx.from_scipy_sparse_matrix(S)
-    spring_pos = nx.spring_layout(G)
-    draw_results(G, spring_pos, partitions, "metis_guess.png")
+def metis_from_pickle(fn, num_partitions):
+    print("Loading pickled data...")
+    S = pickle.load(open("blockchain/{}.pickle".format(fn), "rb"))
+    metis_fn = "blockchain/{}.graph".format(fn)
+    
+    print("Reformatting pickled data...")
+    format_metis(S, metis_fn)
+    return run_metis(metis_fn, num_partitions)
 
 if __name__ == "__main__":
     metis_partition("data_0.000000", num_partitions=25)
