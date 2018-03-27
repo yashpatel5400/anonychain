1f5e14e082b2c75135ae8e688ae02ad7dc460e3a
Yash Patel <ypatel@princeton.edu>
2018-03-26 20:51:10 -0400

    Finished accuracy calcs

--- truth/compare.py
+++ truth/compare.py
@@ -10,9 +10,32 @@ import pickle
 def compute_accuracy(truth_fn, guess_fn):
     truth = pickle.load(open(truth_fn, "rb"))
     guess = pickle.load(open(guess_fn, "rb"))
+    
+    truth_wallets = truth.values()
+    guess_wallets = guess.values()
 
+    guess_performance = {}
+    for guess_wallet in guess_wallets:
+        total_overlap = 0
+        for true_owner in truth:
+            overlap = len(guess_wallet.intersection(truth[true_owner]))
+            total_overlap += overlap
+            if overlap > 0:
+                if guess_wallet not in guess_performance:
+                    guess_performance[guess_wallet] = {}
+                guess_performance[guess_wallet][true_owner] = overlap
+        if total_overlap > 0:
+            for owner in guess_performance[guess_wallet]:
+                guess_performance[guess_wallet] /= total_overlap
+    return guess_performance
+
+def write_performance(truth_fn, guess_fn):
+    guess_performance = compute_accuracy(truth_fn, guess_fn)
+    with open("truth/accuracy.txt", "w") as f:
+        for person in guess_performance:
+            f.write("{} : {}\n".format(person, guess_performance[person]))
 
 if __name__ == "__main__":
-    compute_accuracy(
+    write_performance(
         "output/SpectralClustering_guess.pickle", 
         "truth/extracted.pickle")
