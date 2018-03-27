f1c3808b9db5730120f60b877306268015fe15eb
Yash Patel <ypatel@princeton.edu>
2018-03-27 15:50:38 -0400

    Fixed METIS formatting bugs

--- blockchain/metis.py
+++ blockchain/metis.py
@@ -6,13 +6,18 @@ analysis by the METIS package
 """
 
 import pickle
+from scipy.sparse import dok_matrix
 
 def format_metis(fn):
     print("Loading pickled data...")
     S = pickle.load(open(fn, "rb"))
     rows, cols = S.shape
     
+    edges = dok_matrix.count_nonzero(S) // 2
     with open("blockchain/metis.graph", "w") as f:
+        # header format: #nodes, #edges, 001 (indicates weighted edges)
+        f.write("{} {} 001\n".format(rows, edges))
+
         print("Reformatting pickled data...")
         for i in range(rows):
             cur_node = []
@@ -22,4 +27,4 @@ def format_metis(fn):
             f.write("{}\n".format(" ".join(cur_node)))
 
 if __name__ == "__main__":
-    format_metis("blockchain/data_0.000005.pickle")
+    format_metis("blockchain/data_0.000000.pickle")
