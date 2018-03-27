9cac7916049f3ceba3f0fdf84d7c5c56f25b142c
Yash Patel <ypatel5400@gmail.com>
2018-03-27 16:16:50 -0400

    METIS successfully integrated

--- blockchain/metis.py
+++ blockchain/metis.py
@@ -5,20 +5,21 @@ __description__ = Formats the raw blockchain graph data into the format required
 analysis by the METIS package
 """
 
+import subprocess
 import pickle
+import networkx as nx
 from scipy.sparse import dok_matrix
+import matplotlib
+matplotlib.use('Agg')
 
-def format_metis(fn):
-    print("Loading pickled data...")
-    S = pickle.load(open(fn, "rb"))
+from analysis.deanonymize import draw_results
+
+def format_metis(S, metis_fn):
     rows, cols = S.shape
-    
     edges = dok_matrix.count_nonzero(S) // 2
-    with open("blockchain/metis.graph", "w") as f:
+    with open(metis_fn, "w") as f:
         # header format: #nodes, #edges, 001 (indicates weighted edges)
         f.write("{} {} 001\n".format(rows, edges))
-
-        print("Reformatting pickled data...")
         for i in range(rows):
             cur_node = []
             for j in range(cols):
@@ -26,5 +27,28 @@ def format_metis(fn):
                     cur_node.append("{} {}".format(j+1, int(S[i,j])))
             f.write("{}\n".format(" ".join(cur_node)))
 
+def metis_partition(fn, num_partitions):
+    print("Loading pickled data...")
+    S = pickle.load(open("blockchain/{}.pickle".format(fn), "rb"))
+    metis_fn = "blockchain/{}.graph".format(fn)
+    
+    print("Reformatting pickled data...")
+    format_metis(S, metis_fn)
+
+    pmetis_cmd = ["pmetis", metis_fn, str(num_partitions)]
+    result = subprocess.run(pmetis_cmd)
+
+    result_fn = "{}.part.{}".format(metis_fn, num_partitions)
+    partitions = [set() for _ in range(num_partitions)]
+    with open(result_fn) as f:
+        line_no = 0
+        for line in f:
+            partitions[int(line.strip())].add(line_no)
+            line_no += 1
+
+    G = nx.from_scipy_sparse_matrix(S)
+    spring_pos = nx.spring_layout(G)
+    draw_results(G, spring_pos, partitions, "metis_guess.png")
+
 if __name__ == "__main__":
-    format_metis("blockchain/data_0.000000.pickle")
+    metis_partition("data_0.000000", num_partitions=25)
