c719b86c094d03b3a23be2cee80391b15634235f
Yash Patel <ypatel5400@gmail.com>
2018-04-22 21:15:59 -0400

    Added plotting features

--- accuracy.py
+++ accuracy.py
@@ -14,9 +14,13 @@ import matplotlib.pyplot as plt
 
 import plotly
 import plotly.graph_objs as go
+from collections import defaultdict
 
+from PIL import Image, ImageFont, ImageDraw
 from setup.sbm import create_sbm, create_clusters
 # from analysis.deanonymize import calc_accuracy, deanonymize
+
+fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
 
 def extract_results(accuracy_metric, p, qs):
     alg_accuracies, alg_times = {}, {} 
@@ -40,21 +44,56 @@ def extract_results(accuracy_metric, p, qs):
 
     return alg_accuracies, alg_times
 
-def graph_results(fn, d, p, qs):
-    data = []
-    for alg in d:
-        data.append(go.Scatter(
-            x=qs,
-            y=d[alg],
-            name=alg
-        ))
+def graph_results(fn, d, p, qs, save_static=True):
+    xaxis = "qs"
+    yaxis = fn
+    if yaxis == "times":
+        yaxis = "time (s)"
 
-    layout = dict(
-        xaxis = dict(title = 'qs'),
-        yaxis = dict(title = fn),
-    )
-    fig = dict(data=data, layout=layout)
-    plotly.offline.plot(fig, filename='output/results_{}-{}.html'.format(fn, p))
+    if p is not None:
+        title = "{} vs. {} for p={}".format(xaxis, yaxis, p)
+        out_fn = 'output/results_{}-{}.png'.format(fn, p)
+    else:
+        title = "{} vs. {}".format(xaxis, yaxis)
+        out_fn = 'output/results_{}.png'.format(fn)
+
+    if save_static:
+        fig = plt.figure(figsize=(12, 8))
+        fig.suptitle(title)
+
+        ax = fig.add_subplot(111)
+        for alg in d:
+            plt.plot(qs, d[alg], marker='o', label=alg)
+        
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10),
+          fancybox=True, shadow=True, ncol=6)
+
+        ax.set_xlabel(xaxis)
+        ax.set_ylabel(yaxis)
+
+        plt.savefig(out_fn)
+        plt.close()
+    
+    else:
+        data = []
+        for alg in d:
+            data.append(go.Scatter(
+                x=qs,
+                y=d[alg],
+                name=alg
+            ))
+
+        layout = dict(
+            title = title,
+            xaxis = dict(title = xaxis),
+            yaxis = dict(title = yaxis),
+        )
+        fig = dict(data=data, layout=layout)
+        plotly.offline.plot(fig, filename=out_fn)
+    return out_fn
 
 def conduct_tests(ps, qs, css):
     """Given lists of p probabilities, q probabilities, and lists of cluster sizes,
@@ -99,11 +138,47 @@ def conduct_tests(ps, qs, css):
                 plt.savefig("output/accuracy/p={}_cs={}_{}.png".format(p, cs, label))
                 plt.close()
 
+def create_collage(width, height, listofimages, output_fn):
+    cols = 2
+    rows = 3
+    thumbnail_width = width//cols
+    thumbnail_height = height//rows
+    size = thumbnail_width, thumbnail_height
+    new_im = Image.new('RGB', (width, height))
+    ims = []
+    for p in listofimages:
+        im = Image.open(p)
+        
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype(os.path.join(fonts_path, 'Archivo.ttf'), 16)
+        draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+        
+        im.thumbnail(size)
+        ims.append(im)
+    i = 0
+    x = 0
+    y = 0
+    for col in range(cols):
+        for row in range(rows):
+            print(i, x, y)
+            new_im.paste(ims[i], (x, y))
+            i += 1
+            y += thumbnail_height
+        x += thumbnail_width
+        y = 0
+
+    new_im.save("output/results_{}.png".format(output_fn))
+
 def main():
     files = os.listdir("output")
-    accuracy_metrics = ["purity", "nmi", "rand_ind", "weighted_ri"]
+    accuracy_metrics = ["purity", "nmi", "weighted_ri"]
 
-    for accuracy_metric in accuracy_metrics:
+    alg_scores = {}
+    avg_alg_scores = {}
+    accuracy_weights = [1,3,2]
+    total_weight = sum(accuracy_weights)
+
+    for i, accuracy_metric in enumerate(accuracy_metrics):
         results = [file for file in files if len(file.split(accuracy_metric)) > 1
             and file.split(".")[-1] == "txt"]
         p_to_qs = {}
@@ -116,12 +191,46 @@ def main():
                 p_to_qs[p] = []
             p_to_qs[p].append(q)
 
+        time_files  = []
+        result_files = []
         for p in p_to_qs:
             qs = p_to_qs[p]
             accuracies, times = extract_results(accuracy_metric, p, qs)
-            graph_results(accuracy_metric, accuracies, p, qs)
-            graph_results("times", times, p, qs)
+            for alg in accuracies:
+                if p not in alg_scores:
+                    alg_scores[p] = {}
+                if alg not in alg_scores[p]:
+                    alg_scores[p][alg] = [0] * len(accuracies[alg])
+                for j, accuracy in enumerate(accuracies[alg]):
+                    alg_scores[p][alg][j] += accuracy_weights[i] * accuracy / total_weight
+
+            out_result = graph_results(accuracy_metric, accuracies, p, qs)
+            result_files.append(out_result)
             print("Finished graphing {} for p={}".format(accuracy_metric, p))
+        create_collage(1200 * 2,800 * 3,result_files, accuracy_metric)
+    
+    result_files = []
+    sorted_ps = sorted(alg_scores.keys())
+    for j, p in enumerate(sorted_ps):
+        out_result = graph_results("overall", alg_scores[p], p, p_to_qs[p])
+        result_files.append(out_result)
+
+        for alg in alg_scores[p]:
+            if alg not in avg_alg_scores:
+                avg_alg_scores[alg] = [0] * len(sorted_ps)
+            avg_alg_scores[alg][j] = np.mean(alg_scores[p][alg])
+
+    graph_results("average", avg_alg_scores, None, sorted_ps)
+    create_collage(1200 * 2,800 * 3,result_files, "overall")
 
 if __name__ == "__main__":
-    main()
+    # main()
+    files = [
+        "output/p-0.8_q-0.0/KMeans_p-0.8_q-0.0.png",
+        "output/p-0.8_q-0.0/ManualHierarchical_p-0.8_q-0.0.png",
+        "output/p-0.8_q-0.0/ManualKmeans_p-0.8_q-0.0.png",
+        "output/p-0.8_q-0.0/Metis_p-0.8_q-0.0.png",
+        "output/p-0.8_q-0.0/MiniBatchKMeans_p-0.8_q-0.0.png",
+        "output/p-0.8_q-0.0/SpectralClustering_p-0.8_q-0.0.png"
+    ]
+    create_collage(1200 * 2,800 * 3,files, "p-0.8_q-0.0")
